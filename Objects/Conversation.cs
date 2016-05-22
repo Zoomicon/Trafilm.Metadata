bcd83b8b893389e993151a0954e870d61a95deb7
George Birbilis <GitHub1@zoomicon.com>
2016-05-22 14:12:45 +0300

    - Film and Conversation objects now calculate aggregate data from their child Conversations and L3occurences respectively - Updated diagrams and exported images - Updated NuGet package number (and published)

--- Objects/Conversation.cs
+++ Objects/Conversation.cs
@@ -1,10 +1,11 @@
 ﻿//Project: Trafilm.Metadata (https://github.com/Zoomicon/Trafilm.Metadata)
 //Filename: Conversation.cs
-//Version: 20160503
+//Version: 20160522
 
 using Trafilm.Metadata.Models;
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Trafilm.Metadata
 {
@@ -12,10 +13,41 @@ namespace Trafilm.Metadata
   public class Conversation : ConversationMetadata, IConversation
   {
 
+    #region --- Fields ---
+
+    protected IEnumerable<IL3occurence> l3occurences; //=null
+
+    #endregion
+
     #region --- Properties ---
 
     public IFilm Film { get; set; }
-    public IEnumerable<IL3occurence> L3occurences { get; set; }
+    public IEnumerable<IL3occurence> L3occurences
+    {
+      get
+      {
+        return l3occurences;
+      }
+      set
+      {
+        l3occurences = value;
+
+        //Calculated from L3occurences//
+
+        if (value != null)
+        {
+          L3occurenceCount = value.Count();
+
+          L3languages = value.Aggregate(new List<string>(), (total, next) => { if (!total.Contains(next.L3language)) total.Add(next.L3language); return total; } ).ToArray();
+          L3languagesCount = L3languages.Count();
+
+          L3languageTypes = value.Aggregate(new List<string>(), (total, next) => { if (!total.Contains(next.L3languageType)) total.Add(next.L3languageType); return total; }).ToArray();
+          L3languageTypesCount = L3languageTypes.Count();
+        }
+        else
+          ClearCalculated();
+      }
+    }
 
     #endregion
 
--- Objects/Film.cs
+++ Objects/Film.cs
@@ -1,10 +1,12 @@
 ﻿//Project: Trafilm.Metadata (https://github.com/Zoomicon/Trafilm.Metadata)
 //Filename: Film.cs
-//Version: 20160503
+//Version: 20160522
 
 using Trafilm.Metadata.Models;
 
 using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace Trafilm.Metadata
 {
@@ -12,9 +14,34 @@ namespace Trafilm.Metadata
   public class Film : FilmMetadata, IFilm
   {
 
+    #region --- Fields ---
+
+    protected IEnumerable<IConversation> conversations; //=null
+
+    #endregion
+
     #region --- Properties ---
 
-    public IEnumerable<IConversation> Conversations { get; set; }
+    public IEnumerable<IConversation> Conversations {
+      get
+      {
+        return conversations;
+      }
+      set
+      {
+        conversations = value;
+
+        //Calculated from Conversations//
+
+        if (value != null)
+        {
+          ConversationCount = value.Count();
+          ConversationsDuration = value.Aggregate(TimeSpan.Zero, (total, next) => total.Add(next.Duration ?? TimeSpan.Zero));
+        }
+        else
+          ClearCalculated();
+      }
+    }
 
     #endregion
 
