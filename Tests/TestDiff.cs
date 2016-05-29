bc24d78ce7ec1a01d48831f4b7f6340c909f7a70
George Birbilis <GitHub1@zoomicon.com>
2016-05-29 04:41:23 +0300

    - Added tests for Diff methods' optional parameters

--- Tests/TestDiff.cs
+++ Tests/TestDiff.cs
@@ -16,9 +16,11 @@ namespace Trafilm.Metadata.Tests
     {
       Assert.AreEqual("None", Diff.GetDifference("a", "a"));
       Assert.AreEqual("a -> b", Diff.GetDifference("a", "b"));
+      Assert.AreEqual("a to b", Diff.GetDifference("a", "b", infixChanged: " to "));
 
       CollectionAssert.AreEqual(new string[] { "None" }, Diff.GetDifferences("a", "a"));
       CollectionAssert.AreEqual(new string[] { "- a", "+ b"}, Diff.GetDifferences("a", "b"));
+      CollectionAssert.AreEqual(new string[] { "Removed a", "Added b" }, Diff.GetDifferences("a", "b", prefixAdded: "Added ", prefixRemoved: "Removed "));
     }
 
     [TestMethod]
@@ -26,6 +28,7 @@ namespace Trafilm.Metadata.Tests
     {
       CollectionAssert.AreEqual(new string[] { "None" }, Diff.GetDifferences(new string[] { "a", "b", "c" }, new string[] { "c", "a", "b" }));
       CollectionAssert.AreEqual(new string[] { "- a", "- c", "+ d" }, Diff.GetDifferences(new string[] { "a", "b", "c" }, new string[] { "d", "b" }));
+      CollectionAssert.AreEqual(new string[] { "Removed a", "Removed c", "Added d" }, Diff.GetDifferences(new string[] { "a", "b", "c" }, new string[] { "d", "b" }, prefixAdded: "Added ", prefixRemoved: "Removed "));
     }
 
   }
