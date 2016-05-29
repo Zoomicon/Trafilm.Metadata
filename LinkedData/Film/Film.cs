554a09c45f4a90c15fc89f045622db7bd2327d98
George Birbilis <GitHub1@zoomicon.com>
2016-05-29 03:01:08 +0300

    Implemented calculation of L2dubbedLanguages and L2subtitledLanguages calculated fields of Film

--- LinkedData/Film/Film.cs
+++ LinkedData/Film/Film.cs
@@ -1,6 +1,6 @@
 ﻿//Project: Trafilm.Metadata (https://github.com/Zoomicon/Trafilm.Metadata)
 //Filename: Film.cs
-//Version: 20160527
+//Version: 20160529
 
 using Trafilm.Metadata.Models;
 
@@ -36,13 +36,37 @@ namespace Trafilm.Metadata
         if (value != null)
         {
           ConversationCount = value.Count();
-
           ConversationsDuration = value.Aggregate(TimeSpan.Zero, (total, next) => total.Add(next.Duration ?? TimeSpan.Zero));
 
           //Calculated from Conversations.L3SToccurrences.L3TToccurrences//
 
-          L2dubbedLanguages = new string[] { }; //TODO
-          L2subtitledLanguages = new string[] { }; //TODO
+          IList<string> dubbed = new List<string>();
+          IList<string> subtitled = new List<string>();
+          foreach (Conversation conversation in value)
+            if ((conversation != null) && (conversation.L3SToccurrences != null))
+              foreach (L3SToccurrence l3SToccurrence in conversation.L3SToccurrences)
+                if ((l3SToccurrence != null) && (l3SToccurrence.L3TToccurrences != null))
+                  foreach (L3TToccurrence l3TToccurrence in l3SToccurrence.L3TToccurrences)
+                    if (l3TToccurrence != null)
+                    {
+                      string l2language = l3TToccurrence.L2language;
+                      if (!string.IsNullOrEmpty(l2language))
+                        switch (l3TToccurrence.L2mode)
+                        {
+                          case "Dubbed":
+                            if (!dubbed.Contains(l2language))
+                              dubbed.Add(l2language);
+                            break;
+                          case "Subtitled":
+                            if (!subtitled.Contains(l2language))
+                              subtitled.Add(l2language);
+                            break;
+                        }
+                    }
+          L2dubbedLanguages = dubbed.ToArray();
+          L2subtitledLanguages = subtitled.ToArray();
+
+          //...
         }
         else
           ClearCalculated();
